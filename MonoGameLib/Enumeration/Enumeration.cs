a6cc015c15007799e59f17a88216429fa26405e8
Dan Stow <Dan_Stow@live.co.uk>
2018-06-17 14:00:41 +0100

    Add enumerations

---
+++ MonoGameLib/Enumeration/Enumeration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLib.Enumeration
+{
+    /// <summary>
+    /// Represents if a camera should force the view into the width or the height
+    /// </summary>
+    public enum CameraFixedSideEnum
+    {
+        Width,
+        Height
+    }
+}

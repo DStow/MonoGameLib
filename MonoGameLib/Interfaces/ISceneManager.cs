1793c4aa799da2d5c4f6f1ff9b83621590d1285f
Dan Stow <Dan_Stow@live.co.uk>
2018-07-15 09:08:21 +0100

    Add interfaces for a scene and a scene manager

---
+++ MonoGameLib/Interfaces/ISceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLib.Interfaces
+{
+    public interface ISceneManager : IInitializable
+    {
+        /// <summary>
+        /// Current scene being offered up
+        /// </summary>
+        IScene CurrentScene { get; }
+
+        /// <summary>
+        /// Change the current scene
+        /// </summary>
+        /// <param name="sceneKey">The scene key to change to</param>
+        void SwitchScene(string sceneKey);
+    }
+}

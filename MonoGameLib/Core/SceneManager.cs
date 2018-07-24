ab9f0f69608f7a1e0ae0d87269434699da299c10
Dan Stow <Dan_Stow@live.co.uk>
2018-07-24 19:15:46 +0100

    Add an abstract scene manager

---
+++ MonoGameLib/Core/SceneManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using MonoGameLib.Interfaces;
+
+namespace MonoGameLib.Core
+{
+    public abstract class SceneManager : Interfaces.ISceneManager
+    {
+        public IScene CurrentScene => _currentScene;
+
+        protected IScene _currentScene;
+
+        protected Dictionary<string, IScene> _scenes { get; set; }
+
+        public void Initialize()
+        {
+            _scenes = new Dictionary<string, IScene>();
+
+            CreateScenes(_scenes);
+
+            // Initialise all of the scenes
+            for (int i = 0; i < _scenes.Count; i++)
+            {
+                _scenes.ToList()[0].Value.Initialize();
+            }
+        }
+
+        /// <summary>
+        /// Create the scenes needed for this scene manager
+        /// </summary>
+        /// <param name="scenes"></param>
+        protected abstract void CreateScenes(Dictionary<string, IScene> scenes);
+
+        public IScene GetScene(string sceneKey)
+        {
+            if (_scenes.Keys.Contains(sceneKey))
+                return _scenes[sceneKey];
+            else
+                throw new Exceptions.SceneDoesNotExistException(sceneKey);
+        }
+
+
+        public virtual void LoadContent(ContentManager content)
+        {
+            // Loop through scenes and load the content
+            for(int i =0; i < _scenes.Count; i++)
+            {
+                _scenes.ToList()[0].Value.LoadContent(content);
+            }
+        }
+
+        public virtual void SwitchScene(string sceneKey)
+        {
+            _currentScene = GetScene(sceneKey);
+        }
+    }
+}
---
+++ MonoGameLib/Exceptions/SceneDoesNotExistException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameLib.Exceptions
+{
+    /// <summary>
+    /// This exception is thrown when a scene cannot be found by a scene manager
+    /// </summary>
+    public class SceneDoesNotExistException : ApplicationException
+    {
+        public string SceneKey { get; set; }
+
+        public SceneDoesNotExistException(string sceneKey)
+            : base(String.Format("The scene '{0}' does not exist", sceneKey))
+        {
+            this.SceneKey = sceneKey;
+        }
+    }
+}
